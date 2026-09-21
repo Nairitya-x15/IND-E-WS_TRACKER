9c216469b0164d1afc229d0204af7adea0448ff8
Nairitya <nairitya021@gmail.com>
2026-09-21 19:11:22 +0530

    ready for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Factory Station Tracker",
+  title: "IndE - Workstation session tracking tool",
   description: "Workstation session tracking tool",
 };
 
